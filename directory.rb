6540db80e9357712daf816cae01b52e842e93746
Charlotte <charlotte.feather@yahoo.com>
2017-04-25 19:22:58 +0100

    Completed 8.4

--- directory.rb
+++ directory.rb
@@ -1,29 +1,19 @@
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
 
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(students, letter)
-    students.each_with_index do |student, index|
-      if student[:name].length < 12
-      puts " #{index +1}. #{student[:name]} (#{student[:cohort]} cohort)"
+def print(student)
+
+count =0
+  while count < (student.count)
+      puts " #{count + 1}. #{student[count][:name]} (#{student[count][:cohort]} cohort)"
+      count = count + 1
     end
-  end
+
 end
+
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
@@ -43,5 +33,5 @@ def input_students
 end
 students = input_students
 print_header
-print(students, "A")
+print(students)
 print_footer(students)
