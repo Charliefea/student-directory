164fd60b75b3c5b9f58af8aaabdc187cfa888999
CharlieFea <charlotte.feather@yahoo.com>
2017-04-28 20:53:59 +0000

    Completed 10

--- directory.rb
+++ directory.rb
@@ -1,37 +1,37 @@
-
+@students = []
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(student)
+def print_students_list
   
-  if student.count == 0 
+  if @students.empty?
     puts "There are no students"
   else
   months = []
-  months = student.map {|x| x[:cohort]}
+  months = @students.map {|x| x[:cohort]}
   months = months.uniq
-  count = 1
+  number = 1
   months.each do |month|
-    student.each do |studentdata|
+    @students.each do |studentdata|
       if month == studentdata[:cohort] 
-      string = "#{count} #{studentdata[:name]},(#{studentdata[:cohort]} cohort)"
+      string = "#{number} #{studentdata[:name]},(#{studentdata[:cohort]} cohort)"
           puts string.center(100)
-          count = count + 1
+          number = number + 1
       end
     end
   end 
   end
 end
 
-def print_footer(names)
+def print_footer
   
-  if names.count == 1
+  if @students.count == 1
       puts "Overall, we have a great student"
       
   else
-      puts "Now we have #{names.count} students"
+      puts "Now we have #{@students.count} students"
       
   end 
 end
@@ -40,7 +40,6 @@ def input_students
   months = ['january','february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
   name = gets.chop
   while !name.empty? do
     puts "Which cohort?"
@@ -56,43 +55,51 @@ def input_students
       cohort = gets.chop.downcase
     end
     
-    students << {name: name, cohort: cohort.capitalize.to_sym, }
+    @students << {name: name, cohort: cohort.capitalize.to_sym, }
     
-      if students.count == 1
-      puts "Now we have #{students.count} student"
+      if @students.count == 1
+      puts "Now we have #{@students.count} student"
       name = gets.chop
       else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
       name = gets.chop
       end 
   end
   
-  students
+   @students
 end
 
 def interative_menu
   students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    
-    selection = gets.chomp
-    
-    case selection
+    print_menu()
+    process(gets.chomp) 
+  end
+end
+
+def process(selection)
+  case selection
     when "1"
-      students = input_students
+      @students = input_students
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
+      show_students()
     when "9"
       exit
     else
       "I don't know what you meant, try again"
     end
-  end
 end
-    
 
- interative_menu()
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+   
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+interative_menu()
